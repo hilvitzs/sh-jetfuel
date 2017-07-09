818bebac2d4078829a9cffe87df927bbaaf36cc1
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-07-09 12:58:07 -0600

    breakout addFolder function

--- App/index.js
+++ App/index.js
@@ -1,15 +1,21 @@
 const folderSubmit = document.querySelector('.folder-submit')
 
 
-folderSubmit.addEventListener("click", (event) => {
-  event.preventDefault()
-  const folderInput = document.querySelector('.folder-input').value
-  console.log(folderInput);
-
+const addFolder = (folderInput) => {
   fetch('/api/v1/folders', {
     method: 'POST',
     headers: {'Content-Type':  'application/json'},
     body: JSON.stringify({ name: folderInput })
   })
   .catch(error => console.log('There was an error', error ))
+
+  document.querySelector('.folder-input').value = ""
+
+}
+
+folderSubmit.addEventListener("click", (event) => {
+  event.preventDefault()
+  let folderInput = document.querySelector('.folder-input').value
+
+  addFolder(folderInput)
 })
